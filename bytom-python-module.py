95f344ec86ac334e225125858d05f643348a5673
Erick Pinos <erick@erickpinos.com>
2019-07-07 14:29:13 -0700

    cleaned up variables and added build transaction

--- bytom-python-module.py
+++ bytom-python-module.py
@@ -2,19 +2,14 @@ import requests
 import json
 
 erick_account = '0UKOBDPC00A02'
-erick_key = '0ff578cc3228fe697a47d178081a1d642e677c637f1c9ecb7434af8130873582'
+erick_address = '0ff578cc3228fe697a47d178081a1d642e677c637f1c9ecb7434af8130873582'
 erick_program = '00143acfa10b486c9690c91ef8112f0472774d1c6b05'
 
 mel_account = '0UKOEPK7G0A04'
-mel_key = '9485475a74ce493a6299b44b7488997e9278f32d6aee00b15fa2a07ee4ce2688'
+mel_address = '9485475a74ce493a6299b44b7488997e9278f32d6aee00b15fa2a07ee4ce2688'
 mel_program = '001403fa045e4417f378243c2779f11b8204d5cd82a9'
 
-#data = open('create-account-receiver.json')
-#response = requests.post('http://localhost:9888/list-pubkeys', data=data)
-
-#print(response.text)
-
-#print(response.text)
+gold_id = '82ec80b9f81217b8c912322fb3f505b1e542b4e58cebb7c128081fbe0d8584c2'
 
 #response = requests.post('http://localhost:9888/build-transaction', data=data)
 
@@ -23,32 +18,39 @@ mel_program = '001403fa045e4417f378243c2779f11b8204d5cd82a9'
 #sign-contractLoad-txt-payload = open('sign-contractLoad-tx-payload.json')
 #submit-contractLoad-txt-payload = open('submit-contractLoad-tx-payload.json')
 
-compilation_payload_data = {
+# Compile the Contract
+
+# Make Python Version
+compile_input_python = {
     "contract": "contract LockPosition(expireBlockHeight: Integer, saver: Program, saver_key: PublicKey) locks lockAmount of lockAsset { clause expire(sig: Signature) { verify above(expireBlockHeight)  verify checkTxSig(saver_key, sig) lock lockAmount of lockAsset with saver } }",
     "args": [
         {"integer": 100 },
-	{"string": "00143acfa10b486c9690c91ef8112f0472774d1c6b05"},
-	{"string": "0ff578cc3228fe697a47d178081a1d642e677c637f1c9ecb7434af8130873582"}
+	{"string": erick_program},
+	{"string": erick_address}
     ]
 }
+print(compile_input_python)
 
-with open("compilation-payload.json", "w") as write_file:
-    json.dump(compilation_payload_data, write_file)
+# Make JSON Version
+with open("compile.json", "w") as write_file:
+    json.dump(compile_input_python, write_file)
+compile_input_json = open('compile.json')
+print(compile_input_json)
 
-compilation = open('compilation-payload.json')
+# Compile
+compile_output_json = requests.post('http://localhost:9888/compile', data=compile_input_json)
+print(compile_output_json)
+print(compile_output_json.text)
 
-#compilation_result = {"status":"success","data":{"name":"LockPosition","source":"contract LockPosition(expireBlockHeight: Integer, saver: Program, saver_key: PublicKey) locks lockAmount of lockAsset { clause expire(sig: Signature) { verify above(expireBlockHeight)  verify checkTxSig(saver_key, sig) lock lockAmount of lockAsset with saver } }","program":"200ff578cc3228fe697a47d178081a1d642e677c637f1c9ecb7434af81308735821600143acfa10b486c9690c91ef8112f0472774d1c6b0501647410cd9f697b7bae7cac6900c3c251547ac100c0","params":[{"name":"expireBlockHeight","type":"Integer"},{"name":"saver","type":"Program"},{"name":"saver_key","type":"PublicKey"}],"value":"lockAmount of lockAsset","clause_info":[{"name":"expire","params":[{"name":"sig","type":"Signature"}],"blockheight":["expireBlockHeight"],"values":[{"name":"","program":"saver","asset":"lockAsset","amount":"lockAmount"}]}],"opcodes":"0x0ff578cc3228fe697a47d178081a1d642e677c637f1c9ecb7434af8130873582 0x00143acfa10b486c9690c91ef8112f0472774d1c6b05 0x64 DEPTH 0xcd9f697b7bae7cac6900c3c251547ac1 FALSE CHECKPREDICATE","error":""}}
+# Get the program
+compile_output_python = json.loads(compile_output_json.text)
 
-compilation_result = requests.post('http://localhost:9888/compile', data=compilation)
-print(compilation_result.text)
-with open("compilation-payload.json", "w") as write_file:
-    json.dump(compilation_payload_data, write_file)
+control_program = compile_output_python.get('data').get('program')
+print(control_program)
 
-decoded_hand = json.loads(compilation_result.text)
 
-program = decoded_hand.get('data').get('program')
-print(program)
-build_contractLoad_tx_payload_data = {
+# Built the Transaction
+build_input_python = {
   "base_transaction": None,
   "actions": [
     {
@@ -66,7 +68,7 @@ build_contractLoad_tx_payload_data = {
     {
       "amount": 10000000000,
       "asset_id": "82ec80b9f81217b8c912322fb3f505b1e542b4e58cebb7c128081fbe0d8584c2",
-      "control_program": program,
+      "control_program": control_program,
       "type": "control_program"
     }
   ],
@@ -74,8 +76,14 @@ build_contractLoad_tx_payload_data = {
   "time_range": 1521625823
 }
 
-with open("build-contractLoad-tx-payload.json", "w") as write_file:
-    json.dump(build_contractLoad_tx_payload_data, write_file)
 
-#Compile
-#response = requests.post('http://localhost:9888/compile', data=compilation-payload)
+# Make JSON Version
+with open("build-input.json", "w") as write_file:
+    json.dump(build_input_python, write_file)
+
+build_input_json = open('build-input.json')
+print(build_input_json)
+
+# Build the transaction
+build_output_json = requests.post('http://localhost:9888/build-transaction', data=build_input_json)
+print(build_output_json.text)
